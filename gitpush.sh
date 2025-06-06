d90408ace260eb2797830fdd34391cc923ed2b4f
Siddharth Chawla (Author) <siddharthchawla0410+gitauthor@gmail.com>
2025-06-06 12:56:30 +0530

    Add gitpush script

---
+++ gitpush.sh
@@ -0,0 +1,22 @@
+
+
+KEY=/root/.ssh/id_github
+
+
+agent_output=$(ssh-agent -s)
+echo "$agent_output"
+eval "$agent_output"
+
+ssh-add $KEY
+
+# When we do not wnat to set upstream
+git push origin master
+
+#When we want to set upstream
+#git push -u origin master
+#we can unset the upstream using following command
+#git branch --unset-upstream
+
+# Kill ssh-agent
+echo "Kill ssh-agent"
+ssh-agent -k
